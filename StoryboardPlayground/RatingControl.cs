b15fcee7e6e8e946deea2c672583c5ebd63d0991
Guye, Raphael <raphael.guye@swatchgroup.com>
2018-10-13 22:24:57 +0200

    Create a custom UIStackView RatingControl and add it to the Storyboard

---
+++ StoryboardPlayground/RatingControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace StoryboardPlayground
+{
+    [Register("RatingControl"), DesignTimeVisible(true)]
+    public class RatingControl : UIStackView
+    {
+        public RatingControl(IntPtr p) : base(p) { }
+
+        public RatingControl()
+        {
+            SetupButtons();
+        }
+
+        public override void AwakeFromNib()
+        {
+            SetupButtons();
+        }
+
+        void SetupButtons()
+        {
+            var button = new UIButton()
+            {
+                BackgroundColor = UIColor.Red,
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
+
+            button.HeightAnchor.ConstraintEqualTo(44.0f).Active = true;
+            button.WidthAnchor.ConstraintEqualTo(44.0f).Active = true;
+
+            AddArrangedSubview(button);
+        }
+    }
+}
