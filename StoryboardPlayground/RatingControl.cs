0cbc6d17c47c8acaa6f30e86e33bccbfed0ea63a
Guye, Raphael <raphael.guye@swatchgroup.com>
2018-10-13 23:31:09 +0200

    Add image to background rating buttons and add the logic to select the stars

--- StoryboardPlayground/RatingControl.cs
+++ StoryboardPlayground/RatingControl.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using CoreGraphics;
 using Foundation;
 using UIKit;
@@ -9,6 +11,9 @@ namespace StoryboardPlayground
     [Register("RatingControl"), DesignTimeVisible(true)]
     public class RatingControl : UIStackView
     {
+        Dictionary<UIButton, int> ratingButtons = new Dictionary<UIButton, int>();
+        int ratingSeletected;
+
         public RatingControl(IntPtr p) : base(p) { }
 
         public RatingControl()
@@ -23,16 +28,77 @@ namespace StoryboardPlayground
 
         void SetupButtons()
         {
-            var button = new UIButton()
+            ClearRatingButtons();
+
+            var emptyStar = UIImage.FromBundle("emptyStar");
+            var filledStar = UIImage.FromBundle("filledStar");
+            var highlightedStar = UIImage.FromBundle("highlightedStar");
+
+            for (var i = 1; i <= 5; i++)
             {
-                BackgroundColor = UIColor.Red,
-                TranslatesAutoresizingMaskIntoConstraints = false
-            };
+                var button = new UIButton()
+                {
+                    BackgroundColor = UIColor.Red,
+                    TranslatesAutoresizingMaskIntoConstraints = false
+                };
 
-            button.HeightAnchor.ConstraintEqualTo(44.0f).Active = true;
-            button.WidthAnchor.ConstraintEqualTo(44.0f).Active = true;
+                button.HeightAnchor.ConstraintEqualTo(44.0f).Active = true;
+                button.WidthAnchor.ConstraintEqualTo(44.0f).Active = true;
 
-            AddArrangedSubview(button);
+                button.SetImage(emptyStar, UIControlState.Normal);
+                button.SetImage(filledStar, UIControlState.Selected);
+                button.SetImage(highlightedStar, UIControlState.Highlighted);
+
+                button.TouchUpInside += Button_TouchUpInside;
+
+                AddArrangedSubview(button);
+
+                ratingButtons[button] = i;
+            }
+        }
+
+        void ClearRatingButtons()
+        {
+            foreach(var button in ratingButtons)
+            {
+                RemoveArrangedSubview(button.Key);
+                button.Key.RemoveFromSuperview();
+            }
+            ratingButtons.Clear();
+        }
+
+        void Button_TouchUpInside(object sender, EventArgs e)
+        {
+            var button = sender as UIButton;
+            ratingButtons.TryGetValue(button, out int rating);
+            Debug.WriteLine($"Star {rating} selected");
+
+            if (ratingSeletected == rating)
+            {
+                DeselectAllStars();
+                ratingSeletected = 0;
+            }
+            else
+            {
+                ratingSeletected = rating;
+                SelectButtonsUntil(rating);
+            }
+        }
+
+        void SelectButtonsUntil(int rating)
+        {
+            int i = 1;
+            foreach (var button in ratingButtons.Keys)
+            {
+                if (i > rating) button.Selected = false;
+                else button.Selected = true;
+                i++;
+            }
+        }
+
+        void DeselectAllStars()
+        {
+            foreach (var button in ratingButtons.Keys) button.Selected = false;
         }
     }
 }
